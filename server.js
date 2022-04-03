f5bdde8892e2e97d19ad2ff7a17615ef3c66300b
Ramchandra <thakuri.ram12@gmail.com>
2022-04-03 23:59:36 +0545

    Fixed Heroku deployment issue

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(${__dirname}/front-end/dist/));
+
 app.use(express.static('./dist/anuglar-personal-portfolio'));
 
-app.get('/*', (req, res) => 
-res.sendFile(`./front-end/dist/index.html`)
+app.get('/*', (req, res) =>
+    res.sendFile(`./front-end/dist/index.html`)
 );
 
 // Start the app by listening on the default Heroku port
