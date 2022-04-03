4559c06f74f2bb8ead417ed3d003ef8889d09cbe
Ramchandra <thakuri.ram12@gmail.com>
2022-04-03 23:51:05 +0545

    Fixed Heroku deployment issue
    -- In Heroku, there was typo in `angular` as `anuglar`, so instead of creating new app in Heroko making change in server.js by using that name.

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-personal-portfolio'));
+app.use(express.static('./dist/anuglar-personal-portfolio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-personal-portfolio/'}),
+    res.sendFile('index.html', {root: 'dist/anuglar-personal-portfolio/'}),
 );
 
 // Start the app by listening on the default Heroku port
